a40c6fc07b5d486944372a0509580768b9e09e25
Vandna Garg <vandnagarg@google.com>
2026-06-09 14:51:44 +0000

    add space

--- modules/policy-authz-extension/outputs.tf
+++ modules/policy-authz-extension/outputs.tf
@@ -7,18 +7,18 @@
 
 output "extension_ids" {
   description = "Map of extension names to their unique resource IDs."
-  value = { for k, v in google_network_services_authz_extension.extension : k => v.id }
+  value       = { for k, v in google_network_services_authz_extension.extension : k => v.id }
 }
 
 output "policy_ids" {
   description = "Map of policy names to their unique resource IDs."
-  value = { for k, v in google_network_security_authz_policy.policy : k => v.id }
+  value       = { for k, v in google_network_security_authz_policy.policy : k => v.id }
 }
 
 
 output "policy_extension_map" {
   description = "Maps each policy name to its assigned extension IDs (if CUSTOM action)."
-  value = {
+  value       = {
     for k, v in local.final_policies_config : k => try(v.extension_names, [])
     if try(v.action, "") == "CUSTOM" && ! try(v.iap_enabled, false)
   }
